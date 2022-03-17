7ba9741cbd13bf28481e285ac52fe9f473c0f47b
thexxl <996683064@qq.com>
2022-03-17 12:03:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/boot/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.boot.git;
+
+/**
+ * Project：git-test
+ * Date：2022/3/17
+ * Time：11:58
+ * Description：123
+ *
+ * @author xxl
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("sbsbsbsbsb");
+    }
+}
