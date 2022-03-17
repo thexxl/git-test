0777de7a7bf6cf6bd2faabbdab16897bda1d7fc6
thexxl <996683064@qq.com>
2022-03-17 13:47:21 +0800

    66666666

--- src/main/java/com/atguigu/boot/git/GitTest.java
+++ src/main/java/com/atguigu/boot/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("sbsbsbsbsb2");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull test1");
     }
 }
